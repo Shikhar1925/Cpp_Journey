5c830751229406811026d61c17a4a68d1e9443ef
Shikhar Bhasin <shikharbhasin.58@gmail.com>
2021-03-06 13:21:36 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//Size of the Datatypes
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Size of short Integer: " << sizeof(short int) << "-bytes";
+    cout << "\nSize of unsigned short Integer: " << sizeof(unsigned short int) << "-bytes";
+    cout << "\nSize of Integer: " << sizeof(int) << "-bytes";
+    cout << "\nSize of long Integer: " << sizeof(long int) << "-bytes";
+    cout << "\nSize of long long Integer: " << sizeof(long long int) << "-bytes";
+    cout << "\nSize of Float: " << sizeof(float) << "-bytes";
+    cout << "\nSize of Double: " << sizeof(double) << "-bytes";
+    cout << "\nSize of long Double: " << sizeof(long double) << "-bytes";
+    cout << "\nSize of Character: " << sizeof(char) << "-bytes";
+    cout << "\nSize of wchar_t: " << sizeof(wchar_t) << "-bytes";
+}
