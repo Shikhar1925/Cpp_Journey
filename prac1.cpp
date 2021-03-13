b8cb6c0c915f241849edb822c5726e104a23c2e9
Shikhar Bhasin <shikharbhasin.58@gmail.com>
2021-03-13 15:39:27 +0530

    init

---
+++ prac1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int pounds, pence, shillings;
+    cout << "Enter the Pounds: ";
+    cin >> pounds;
+    cout << "Enter the Shillings: ";
+    cin >> shillings;
+    cout << "Enter the pence: ";
+    cin >> pence;
+    float totalpence;
+    totalpence = (float)((shillings * 12) + pence) / 240;
+    pence = totalpence * 100;
+    cout << totalpence << endl;
+    if (pence >= 100)
+    {
+        pounds = pounds + (pence / 100);
+        pence = pence % 100;
+    }
+    cout << "Decimal Pound: " << pounds << "." << pence;
+    return 0;
+}
